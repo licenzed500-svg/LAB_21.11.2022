9473658f69018b68f941f4bb090ea62c6e9587d3
Maksim Merzhew <79693216+licenzed500-svg@users.noreply.github.com>
2022-11-21 14:28:26 +0300

    Create task_118.cpp

---
+++ task_118.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <time.h>
+
+
+
+int get_arr_size()
+{
+    unsigned short arrsize = 0;
+    std::cout << "enter array size:";
+    std::cin >> arrsize;
+    system("cls");
+    return arrsize;
+}
+
+void get_elements(int* arr, short arrsize)
+{
+    short min = 0;
+    short max = 5;
+    for(short i = 0; i<arrsize; i++)
+    {
+        arr[i] = min + rand() % (max-min);
+    }
+}
+
+void change_element(int* arr, short arrsize)
+{
+    for(short i = 0; i<arrsize; i++)
+    {
+        if( (i%2!=0) && (arr[i] == 3) )
+        {
+            arr[i]*=i;
+        }
+    }
+}
+
+void array_out(int* arr, short arrsize)
+{
+    for(short i = 0; i<arrsize; i++)
+    {
+       std::cout << arr[i] << "\n";
+    }
+}
+int main()
+{
+    srand(time(NULL));
+    const unsigned short arrsize = get_arr_size();
+    int arr[arrsize];
+    get_elements(arr, arrsize);
+    change_element(arr, arrsize);
+    array_out(arr, arrsize);
+    return 0;
+
+}
